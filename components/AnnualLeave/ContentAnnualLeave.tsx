2f569721a166e8ffaaddc2eba1907c0a0d9da037
Chaewon Kim <chaewonkim8@gmail.com>
2023-09-19 16:25:34 +0900

    major bug fix. updated the calculation logic in the annual leave calculator

--- components/AnnualLeave/ContentAnnualLeave.tsx
+++ components/AnnualLeave/ContentAnnualLeave.tsx
@@ -24,45 +24,50 @@ export default function ContentAnnualLeave() {
         console.log(boxType);
     };
 
+    // const calculateYearsOfService = (startDate: Date | null, endDate: Date | null): number => {
+    //     if (startDate && endDate) {
+    //         const yearsDiff = endDate.getFullYear() - startDate.getFullYear();
+    //         const monthsDiff = endDate.getMonth() - startDate.getMonth();
+    //         const daysDiff = endDate.getDate() - startDate.getDate();
+
+    //         let years = yearsDiff;
+
+    //         // Check if end date is earlier in the same month
+    //         if (monthsDiff === 0 && daysDiff < 0) {
+    //             years -= 1;
+    //             const lastMonthDays = new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate();
+    //             const remainingDays = lastMonthDays - startDate.getDate() + 1 + endDate.getDate();
+    //             return years + (remainingDays / lastMonthDays) / 12;
+    //         }
+
+    //         const startDateDaysInMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0).getDate();
+    //         return years + (monthsDiff + daysDiff / startDateDaysInMonth) / 12; // Consider partial months as fractions of a year
+    //     }
+
+    //     return 0;
+    // };
     const calculateYearsOfService = (startDate: Date | null, endDate: Date | null): number => {
         if (startDate && endDate) {
             const yearsDiff = endDate.getFullYear() - startDate.getFullYear();
             const monthsDiff = endDate.getMonth() - startDate.getMonth();
             const daysDiff = endDate.getDate() - startDate.getDate();
-
-            let years = yearsDiff;
-
-            // Check if end date is earlier in the same month
-            if (monthsDiff === 0 && daysDiff < 0) {
-                years -= 1;
-                const lastMonthDays = new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate();
-                const remainingDays = lastMonthDays - startDate.getDate() + 1 + endDate.getDate();
-                return years + (remainingDays / lastMonthDays) / 12;
-            }
-
+    
+            // Calculate the total number of months between start and end dates
+            const totalMonths = yearsDiff * 12 + monthsDiff;
+    
+            // Calculate the fraction of a year using actual days in each month
             const startDateDaysInMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0).getDate();
-            return years + (monthsDiff + daysDiff / startDateDaysInMonth) / 12; // Consider partial months as fractions of a year
+            const partialYear = (monthsDiff + daysDiff / startDateDaysInMonth) / 12;
+    
+            return yearsDiff + partialYear;
         }
-
+    
         return 0;
     };
-    // Function to calculate entitled annual leaves
-    // const calculateEntitledAnnualLeaves = (yearsOfService: number): number => {
-    //     if (yearsOfService < 1) {
-    //         return 0; // No entitlement for less than 1 year of service
-    //     }
-
-    //     if (yearsOfService <= 2) {
-    //         return 7; // 7 days for the first 2 years
-    //     } else if (yearsOfService < 9) {
-    //         return 7 + (yearsOfService - 2); // 7 days for the first 2 years, plus 1 additional day for each year from year 3 to year 8
-    //     } else {
-    //         return 12; // 12 days for 9 or more years of service
-    //     }
-    // };
+    
+    
 
     // Function to calculate entitled annual leaves with consideration for partial years
-
     const calculateEntitledAnnualLeaves = (yearsOfService: number): number => {
         // Calculate annual leave entitlement for a full year
         const annualLeavePerYear = (year: number) => {
@@ -75,20 +80,22 @@ export default function ContentAnnualLeave() {
             }
         };
 
-        // Calculate the integer part (full years) and fractional part (partial year)
-        const fullYears = Math.floor(yearsOfService);
-        const partialYear = yearsOfService - fullYears;
+        // // Calculate the integer part (full years) and fractional part (partial year)
+        // const fullYears = Math.floor(yearsOfService);
+        // const partialYear = yearsOfService - fullYears;
 
-        // Calculate annual leave for full years
-        let entitledForFullYears = 0;
-        for (let year = 1; year <= fullYears; year++) {
-            entitledForFullYears += annualLeavePerYear(year);
-        }
+        // // Calculate annual leave for full years
+        // let entitledForFullYears = 0;
+        // for (let year = 1; year <= fullYears; year++) {
+        //     entitledForFullYears += annualLeavePerYear(year);
+        // }
 
-        // Calculate annual leave for the partial year as a fraction of a full year
-        const entitledForPartialYear = partialYear * annualLeavePerYear(fullYears + 1);
+        // // Calculate annual leave for the partial year as a fraction of a full year
+        // const entitledForPartialYear = partialYear * annualLeavePerYear(fullYears + 1);
 
-        return entitledForFullYears + entitledForPartialYear;
+        // return entitledForFullYears + entitledForPartialYear;
+        const entitledAnnualLeaves = annualLeavePerYear(yearsOfService)
+        return entitledAnnualLeaves;
     };
 
     // Function to calculate accumulated annual leaves
@@ -102,6 +109,8 @@ export default function ContentAnnualLeave() {
         // Calculate annual leave for full years
         for (let year = 1; year <= fullYears; year++) {
             accumulatedLeaves += calculateEntitledAnnualLeaves(year);
+            console.log(year);
+            console.log(accumulatedLeaves);
         }
 
         // Calculate annual leave for the partial year as a fraction of a full year
@@ -109,21 +118,6 @@ export default function ContentAnnualLeave() {
 
         return accumulatedLeaves;
     };
-
-
-
-    // Function to calculate accumulated annual leaves
-    // const calculateAccumulatedAnnualLeaves = (yearsOfService: number): number => {
-    //     let accumulatedLeaves = 0;
-
-    //     for (let year = 1; year <= yearsOfService; year++) {
-    //         accumulatedLeaves += calculateEntitledAnnualLeaves(year);
-    //     }
-
-    //     return accumulatedLeaves;
-    // };
-
-    // Source of information: https://www.labour.gov.hk/eng/faq/cap57i_whole.htm
 
     useEffect(() => {
         if (startDate && endDate) {
